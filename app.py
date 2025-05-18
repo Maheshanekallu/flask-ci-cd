73bfe0f73ba5711a39ed9e7f3cc2ad5dcba89516
Mahesh Naik <maheshanekallu@gmail.com>
2025-05-18 13:46:51 +0530

    Fix merge conflicts in requirements.txt

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from flask import Flask
-<<<<<<< HEAD
 app = Flask(__name__)
-=======
 import os
 
 secret_key = os.getenv('SECRET_KEY')
@@ -9,18 +7,12 @@ debug_mode = os.getenv('DEBUG')
 app = Flask(__name__)
 app.secret_key = os.getenv("SECRET_KEY")
 
->>>>>>> cd5ac3b (Initial commit without secret)
-
 @app.route("/")
 def hello():
     return "Hello from Flask CI/CD!"
-<<<<<<< HEAD
-=======
 @app.route('/health')
 def health():
     return 'OK', 200
-
->>>>>>> cd5ac3b (Initial commit without secret)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
