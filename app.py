43c0a89795f1d8271babfece4de7809546022e15
Mahesh Naik <maheshanekallu@gmail.com>
2025-05-22 23:33:22 +0530

    Commited

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, redirect, render_template_string, url_for, jso
 import os
 import requests
 
+port = int(os.environ.get("PORT", 5000))
 app = Flask(__name__)
 app.secret_key = os.getenv("SECRET_KEY", "default_secret")
 
@@ -108,4 +109,4 @@ def github_webhook():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=port)
